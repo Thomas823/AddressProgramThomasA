fadab27f3489c880081bec95bd262c1f59e587ee
Thomas823 <35612259+Thomas823@users.noreply.github.com>
2018-02-16 12:34:26 -0500

    Initial commit

---
+++ AddressProgramThomasA/AddressProgramForm.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace AddressProgramThomasA
+{
+    partial class frmAddressProgram
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblCity = new System.Windows.Forms.Label();
+            this.lblCountry = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.ForeColor = System.Drawing.Color.Crimson;
+            this.lblName.Location = new System.Drawing.Point(23, 41);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(240, 39);
+            this.lblName.TabIndex = 0;
+            this.lblName.Text = "Thomas Aubin";
+            // 
+            // lblCity
+            // 
+            this.lblCity.AutoSize = true;
+            this.lblCity.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCity.Location = new System.Drawing.Point(89, 110);
+            this.lblCity.Name = "lblCity";
+            this.lblCity.Size = new System.Drawing.Size(107, 33);
+            this.lblCity.TabIndex = 1;
+            this.lblCity.Text = "Ottawa";
+            // 
+            // lblCountry
+            // 
+            this.lblCountry.AutoSize = true;
+            this.lblCountry.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCountry.ForeColor = System.Drawing.Color.DarkViolet;
+            this.lblCountry.Location = new System.Drawing.Point(85, 178);
+            this.lblCountry.Name = "lblCountry";
+            this.lblCountry.Size = new System.Drawing.Size(116, 33);
+            this.lblCountry.TabIndex = 2;
+            this.lblCountry.Text = "Canada";
+            this.lblCountry.Click += new System.EventHandler(this.lblCountry_Click);
+            // 
+            // frmAddressProgram
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Cyan;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.lblCountry);
+            this.Controls.Add(this.lblCity);
+            this.Controls.Add(this.lblName);
+            this.ForeColor = System.Drawing.Color.OrangeRed;
+            this.Name = "frmAddressProgram";
+            this.Text = "Address Program, by Thomas";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblCity;
+        private System.Windows.Forms.Label lblCountry;
+    }
+}
+
